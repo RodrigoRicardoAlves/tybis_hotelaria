a2ca6339240ef8a5a9b999df870be29163bbdfd9
rodrigo <rodrigo@rodrigoricardo.eng.br>
2026-01-03 09:12:54 -0300

    criado login.html template and updated urls modificado de core para ''

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     # Login/Logout Padrão
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     # Painel Principal
